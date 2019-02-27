fb6d67c62459b441ec1dd101a4e110b5f183c7d9
pjqdyd <1909025079@qq.com>
2019-02-27 20:11:24 +0800

    --init--

---
+++ src/main/java/com/pjqdyd/azuredemo/AzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pjqdyd.azuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjqdyd/azuredemo/AzureDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pjqdyd.azuredemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AzureDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
